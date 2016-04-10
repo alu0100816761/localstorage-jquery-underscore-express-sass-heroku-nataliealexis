1f12be9769cefd0201ba88a4b320fa9fdfefae14
alu0100816761 <alu0100816761@ull.edu.es>
2016-04-10 11:32:06 +0100

    Actualizada nueva ruta

---
+++ minified/index-min.js
@@ -0,0 +1,1 @@
+var express=require("express"),app=express();app.set("port",process.env.PORT||5e3),app.use(express["static"](__dirname+"/public")),app.set("views",__dirname+"/views"),app.set("view engine","ejs"),app.get("/",function(e,p){p.render("index")}),app.listen(app.get("port"),function(){console.log("Node app is running on port",app.get("port"))});
